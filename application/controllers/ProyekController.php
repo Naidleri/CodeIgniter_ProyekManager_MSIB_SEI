6eb61c7b1e13efbd43ef93fdb2c7f1642f7f20e4
haris ardian <harisfirman387@gmail.com>
2024-08-18 07:25:19 +0700

    Fix : 'edit_proyek' method to handle 'proyek by id'

--- application/controllers/ProyekController.php
+++ application/controllers/ProyekController.php
@@ -79,16 +79,25 @@ class ProyekController extends CI_Controller {
     }
 
     public function edit_proyek($id) {
-        $data['proyek'] = json_decode($this->http_request('http://localhost:8080/proyek/' . $id));
-        $this->load->view('edit_proyek_view', $data);
+        $all_proyek = json_decode($this->http_request('http://localhost:8080/proyek'));
+        $data['proyek'] = array_filter($all_proyek, function($p) use ($id) {
+            return $p->id == $id;
+        });
+        
+        if (empty($data['proyek'])) {
+            show_404();
+        }
+        
+        $data['proyek'] = array_values($data['proyek'])[0];
+        
+        $data['lokasi'] = json_decode($this->http_request('http://localhost:8080/lokasi'));
+        
+        $this->load->view('edit_proyek', $data);
     }
-
+    
+    
     public function update_proyek($id) {
         $data = $this->input->post();
-        $data['lokasiList'] = array_map(function($id) {
-            return ['id' => $id];
-        }, $data['lokasi']);
-        unset($data['lokasi']);
         $this->http_request('http://localhost:8080/proyek/' . $id, 'PUT', $data);
         redirect('/');
     }
